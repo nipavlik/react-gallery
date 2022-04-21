28cae59c80f4c0d8053cb2216d44f9f8d2bd7ba8
Pavel Nikolaev <52615565+nipavlik@users.noreply.github.com>
2022-04-21 15:53:58 +1000

    Saving the menu on reboot

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,29 +1,38 @@
 import { Menu as AntMenu } from "antd";
 import { UserOutlined, PictureOutlined } from "@ant-design/icons";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from "react-router-dom";
 
 const paths = {
-  'about': {
-    path: "/about"
+  about: {
+    path: "/about",
   },
-  'all': {
-    path: "/"
-  }
-}
+  all: {
+    path: "/",
+  },
+};
 
 function Menu() {
-
   const navigate = useNavigate();
+  const location = useLocation();
 
   const onSelect = ({ key }) => {
-    navigate(paths[key].path)
-  }
+    navigate(paths[key].path);
+  };
+
+  const getSelectedKeys = () => {
+    if (location.pathname === "/") {
+      return ["all"];
+    }
+    if (location.pathname === "/about") {
+      return ["about"];
+    }
+  };
 
   return (
     <AntMenu
       theme="dark"
       mode="inline"
-      defaultSelectedKeys={["1"]}
+      defaultSelectedKeys={getSelectedKeys()}
       onClick={onSelect}
       items={[
         {
